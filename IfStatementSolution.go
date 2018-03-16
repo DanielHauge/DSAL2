b4c1ed91c1c2682a372f8a4c1b5b2317e53471f5
DanielHauge <Animcuil@gmail.com>
2018-03-16 12:57:41 +0100

    Push

--- IfStatementSolution.go
+++ IfStatementSolution.go
@@ -1,3 +1,94 @@
 package main
 
+func IfElse (Answers []bool)string{
+	results := ""
+
+	// First : Hand ins made in time
+	// Second : Attend Lectures
+	// Third : Read books
+	// Fourth : Make exercises
+
+
+	if Answers[0]{ // First Layer
+		if Answers[1]{ // Second Layer
+			if Answers[2]{ // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return SuccedString()
+				} else { // Fourth Layer
+					return SuccedString()
+				}
+			} else { // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return SuccedString()
+				} else { // Fourth Layer
+					return SuccedString()
+				}
+			}
+		} else { // Second Layer
+			if Answers[2]{ // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return SuccedString()
+				} else { // Fourth Layer
+					return FailString()
+				}
+			} else { // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return FailString()
+				} else { // Fourth Layer
+					return FailString()
+				}
+			}
+		}
+	} else { // First Layer
+
+		if Answers[1]{ // Second Layer
+			if Answers[2]{ // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return FailString()
+				} else { // Fourth Layer
+					return FailString()
+				}
+			} else { // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return FailString()
+				} else { // Fourth Layer
+					return FailString()
+				}
+			}
+		} else { // Second Layer
+			if Answers[2]{ // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return FailString()
+				} else { // Fourth Layer
+					return FailString()
+				}
+			} else { // Third Layer
+				if Answers[3]{ // Fourth Layer
+					return FailString()
+				} else { // Fourth Layer
+					return FailString()
+				}
+			}
+		}
+	}
+
+
+	return results
+}
+
+func SmallIfElse (Answers []bool)string{
+
+	if !Answers[0]{
+		return FailString()
+	} else if !Answers[1]&&!Answers[2]{
+		return FailString()
+	} else if !Answers[1]&&!Answers[3]{
+		return FailString()
+	} else {
+		return SuccedString()
+	}
+
+
+}
+
 
--- Main.go
+++ Main.go
@@ -1,4 +1,33 @@
 package main
 
+
+
 func main() {
+
+/* If-Else
+/////////// Binary IF elses
+	log.Println(IfElse([]bool{true,true,true,true}))
+	log.Println(IfElse([]bool{true,true,false,true}))
+	log.Println(IfElse([]bool{true,true,true,false}))
+	log.Println(IfElse([]bool{true,false,true,true}))
+	log.Println(IfElse([]bool{true,false,false,true}))
+
+///////////// Short IfElse
+	log.Println(SmallIfElse([]bool{true,true,true,true}))
+	log.Println(SmallIfElse([]bool{true,true,false,true}))
+	log.Println(SmallIfElse([]bool{true,true,true,false}))
+	log.Println(SmallIfElse([]bool{true,false,true,true}))
+	log.Println(SmallIfElse([]bool{true,false,false,true}))
+*/
+
+
+
+}
+
+func FailString()string{
+	return "You could easily fail the exam"
+}
+
+func SuccedString()string{
+	return "You should be able to pass the exam"
 }
